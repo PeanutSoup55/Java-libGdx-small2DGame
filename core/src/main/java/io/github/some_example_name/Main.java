46f1b447f050e1978400e664b99cbdc5f16abd75
Eric Blanchette <ericblanchette55hockey@gmail.com>
2026-02-10 22:19:31 -0500

    Added sprite sheet of character and player class

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -7,26 +7,26 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    private static final float WORLD_WIDTH = 1200;
+    private static final float WORLD_HEIGHT = 900;
+    private static final Player.RenderMode RENDER_MODE = Player.RenderMode.ISOMETRIC;
     private SpriteBatch batch;
-    private Texture image;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
     }
 }
---
+++ core/src/main/java/io/github/some_example_name/Player.java
@@ -0,0 +1,13 @@
+package io.github.some_example_name;
+
+public class Player {
+    private static final int FRAME_WIDTH = 16;
+    private static final int FRAME_HEIGHT = 16;
+    private static final float ANIMATION_SPEED = 0.15f;
+    private static final float MOVE_SPEED = 100f;
+
+    public enum RenderMode{
+        TOP_DOWN,
+        ISOMETRIC
+    }
+}
